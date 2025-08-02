21df43dd389b5220f4952ee05cbd1ce2d4e2769b
Aathif-X <Aathif.2501@xcelevate.org>
2025-08-02 16:21:26 +0530

    Add files via upload

---
+++ Aathif.2501/java/flowcharts/Fcp004.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Fcp004{
+public static void main (String [] args){
+Scanner sc= new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println((float)a/b);
+System.out.println(a%b);
+
+}
+
+
+
+}
