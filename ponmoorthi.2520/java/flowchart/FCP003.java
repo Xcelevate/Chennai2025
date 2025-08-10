8320233a35f5b5297852042990d3f1ecdbdd645f
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-08-10 16:27:44 +0530

    FCP003

---
+++ ponmoorthi.2520/java/flowchart/FCP003.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FCP003{
+public static void main(String a[]){
+Scanner sc=new Scanner(System.in);
+int b=sc.nextInt();
+int d=sc.nextInt();
+int temp=b;
+b=d;
+d=temp;
+System.out.println(b);
+System.out.println(d);
+}}
