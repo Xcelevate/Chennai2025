a8572e4966a98d90b1603c47443ff561fbe0e17e
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-08-10 16:25:44 +0530

    FCP001

---
+++ ponmoorthi.2520/java/flowchart/FCP001.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class FCP001{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+int day=sc.nextInt();
+sc.nextLine();
+String month=sc.nextLine();
+int year=sc.nextInt();
+System.out.println(day);
+System.out.println(month);
+System.out.println(year);
+}
+}
+
