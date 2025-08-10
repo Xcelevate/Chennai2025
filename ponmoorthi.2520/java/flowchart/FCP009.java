17342609b15b381e68d88e17e0528f19cfef03a3
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-08-10 16:32:40 +0530

    FCP009

---
+++ ponmoorthi.2520/java/flowchart/FCP009.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FCP009{
+public static void main(String a[]){
+Scanner s=new Scanner(System.in);
+int x=s.nextInt();
+int y=s.nextInt();
+if(x>y){
+int k=x/y;
+int m=y*k;
+System.out.println(m);
+}
+}}
