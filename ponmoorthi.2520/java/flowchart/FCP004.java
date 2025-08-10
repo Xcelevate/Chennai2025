9456c255ad8762d9e9c8f2c326b512a61fba1924
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-08-10 16:28:30 +0530

    FCP004

---
+++ ponmoorthi.2520/java/flowchart/FCP004.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FCP004{
+public static void main(String a[])
+{
+Scanner s=new Scanner(System.in);
+int b=s.nextInt();
+int c=s.nextInt();
+System.out.println(b+c);
+System.out.println(b-c);
+System.out.println(b*c);
+System.out.println((float)((float)b/c));
+System.out.println(b/c);
+System.out.println(b%c);
+}}
