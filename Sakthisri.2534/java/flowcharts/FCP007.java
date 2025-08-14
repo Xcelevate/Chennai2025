e7aa08b9ce2c48fd98c3b91392ed2dd083ba44ef
Sri here <sakthisri.2534@xcelevate.org>
2025-08-14 12:27:59 +0530

    Add files via upload

---
+++ Sakthisri.2534/java/flowcharts/FCP007.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class FCP007{
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+float C=input.nextFloat();
+float F=(float)((9*C)/5)+32;
+System.out.printf("%.2f",F);
+}
+}
