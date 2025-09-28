b8178cab07910d2c83a067ec5fc11bd516c67b68
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-09-28 11:23:59 +0530

    FCP011

---
+++ Charumathi.2507/java/flowcharts/FCP011.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FCP011{
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int A=Sc.nextInt();
+if(A%2 ==0){
+System.out.print("Even");
+}
+else{
+System.out.print("Odd");
+}
+}
+}
