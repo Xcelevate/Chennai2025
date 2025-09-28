004192e7974bd17ef77d1f95294d047745f53e28
XcelevatorCharumathi <charumathi.2507@xcelevate.org>
2025-09-28 11:25:51 +0530

    FCP013

---
+++ Charumathi.2507/java/flowcharts/FCP013.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FCP013{
+public static void main(String []args){
+Scanner Sc=new Scanner(System.in);
+int n=Sc.nextInt();
+if(n%2==0){
+System.out.println(n);
+}
+else{
+int R = n*2;
+System.out.println(R);
+}
+}
+}
