4a6fc088c0ed6f2cbf7fb4c8090ea59abb41784f
XcelevatorAkshaya <akshaya.2503@xcelevate.org>
2025-10-01 14:30:22 +0530

    Fcp001

---
+++ Akshaya.2503/java/flowcharts/Fcp001.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Fcp001{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int birth_date=sc.nextInt();
+sc.nextLine();
+String birth_month=sc.nextLine();
+int birth_year=sc.nextInt();
+System.out.println(birth_date);
+System.out.println(birth_month);
+System.out.println(birth_year);
+}
+}
+
+
