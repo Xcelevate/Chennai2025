dea5eb33bd574e2f5c37aa9f7f7d2bac2e68ba20
XcelevatorKeerthana <keerthana.2513@xcelevate.org>
2025-10-02 07:22:31 +0530

    FCP011

---
+++ Keerthana.2513/java/flowcharts/FCP011.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FCP011{
+public static void main(String[]args){
+Scanner sc =new Scanner(System.in);
+int k=sc.nextInt();
+if(k%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}}}
