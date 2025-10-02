26a98650c71aea9835e35485f9e986d035fab89a
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-10-02 09:46:38 +0530

    FCP061

---
+++ Keerthika.2514 /java/flowcharts/FCP061.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class FCP061{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int N=sc.nextInt();
+int a[]=new int[N];
+for(int i=0;i<N;i++){
+	a[i]=sc.nextInt();
+}
+int count=1;
+for(int j=0;j<N;j++){
+	for(int k=j+1;k<N;k++){
+		if(a[j]==a[k]){
+			count=count+1;
+			a[k]=-1;
+		}
+	}
+	if(count>=1&&a[j]!=-1){
+		System.out.println(a[j]+" - "+count);
+	}
+count=1;
+}
+}}
