3d3079cdfb88b2fa78716cc605d0949da71ebc32
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-10-02 07:13:02 +0530

    FCP013

---
+++ Keerthika.2514 /java/flowcharts/Fcp013.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Fcp013{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%2==0){
+System.out.println(a);
+}
+else{
+int b=a*2;
+System.out.println(b);
+}
+}
+}
