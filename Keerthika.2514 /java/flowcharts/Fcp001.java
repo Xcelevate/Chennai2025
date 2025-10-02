170d66e414d68b3c334c53e46a6fb27ad7a870ec
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-10-02 06:59:32 +0530

    FCP001

---
+++ Keerthika.2514 /java/flowcharts/Fcp001.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Fcp001{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int birthdate=sc.nextInt();
+sc.nextLine();
+String birthmonth=sc.nextLine();
+int birthyear=sc.nextInt();
+System.out.println(birthdate);
+System.out.println(birthmonth);
+System.out.println(birthyear);
+}
+}
+
