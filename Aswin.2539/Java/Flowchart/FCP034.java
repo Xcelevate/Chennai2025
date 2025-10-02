b6756ce50dc0f6441b18fbd5b5af1c918cf623a9
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-10-02 10:21:22 +0530

    FCP034

---
+++ Aswin.2539/Java/Flowchart/FCP034.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FCP034{
+public static void main(String [] args){
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+for(int i=1;i<=n;i++){
+if(n%i==0){
+System.out.println(i);}}
+}
+}
