398fec066df10733d65fa5bdff8537ce4a7a36d8
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-10-02 09:55:58 +0530

    14

---
+++ ponmoorthi.2520/java/flowchart/FCP014.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class FCP014{
+public static void main(String a[]){
+Scanner s=new Scanner(System.in);
+int x=s.nextInt();
+int y=s.nextInt();
+int z=s.nextInt();
+int max=0;
+
+if(x>y){
+max=x;
+}
+else{
+max=y;
+}
+if(max>z){
+System.out.println(max);
+}
+else{
+System.out.println(z);
+}}}
