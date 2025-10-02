27e2df6d11811b29aab970d9075f7ee8b0fba5ab
Xcelevator-Sreeganesh <SreeGanesh.2531@xcelevate.org>
2025-10-02 11:55:32 +0530

    Add files via upload

---
+++ SreeGanesh.2531/java/flowchart/Fcp001.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Fcp001{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int d=scn.nextInt();
+scn.nextLine();
+String m=scn.nextLine();
+int y=scn.nextInt();
+System.out.println(d);
+System.out.println(m);
+System.out.println(y);
+}
+}import java.util.Scanner;
+public class Fcp001{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int d=scn.nextInt();
+scn.nextLine();
+String m=scn.nextLine();
+int y=scn.nextInt();
+System.out.println(d);
+System.out.println(m);
+System.out.println(y);
+}
+}import java.util.Scanner;
+public class Fcp001{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int d=scn.nextInt();
+scn.nextLine();
+String m=scn.nextLine();
+int y=scn.nextInt();
+System.out.println(d);
+System.out.println(m);
+System.out.println(y);
+}
+}import java.util.Scanner;
+public class Fcp001{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int d=scn.nextInt();
+scn.nextLine();
+String m=scn.nextLine();
+int y=scn.nextInt();
+System.out.println(d);
+System.out.println(m);
+System.out.println(y);
+}
+}
