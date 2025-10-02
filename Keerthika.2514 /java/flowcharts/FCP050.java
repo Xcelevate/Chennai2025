1ddfcdf001833eb9de814e6bb71fe978346d77ec
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-10-02 09:37:30 +0530

    FCP050

---
+++ Keerthika.2514 /java/flowcharts/FCP050.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FCP050{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int N=sc.nextInt();
+for(int i=1;i<=N;i++){
+	if(N%i==0){
+		int count=0;
+		for(int j=1;j<=i;j++){
+			if(i%j==0){
+				count=count+1;
+			}
+		}
+	
+	if(count==2){
+		System.out.println(i);
+	}
+	}
+}
+}}
