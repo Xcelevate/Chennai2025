ffacfb25181bab9435870c0df81b203043b054dd
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-10-02 09:51:30 +0530

    FCP073

---
+++ Keerthika.2514 /java/flowcharts/FCP073.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class FCP073{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+int[][]a=new int[m][m];
+int[][]b=new int[m][m];
+for(int i=0;i<m;i++){
+	for(int j=0;j<m;j++){
+	 	a[i][j]=sc.nextInt();
+	}
+}
+for(int i=0;i<m;i++){
+	for(int j=0;j<m;j++){
+	 	b[i][j]=sc.nextInt();
+	}
+}
+for(int i=0;i<m;i++){
+	for(int j=0;j<m;j++){
+		System.out.println(a[i][j]+b[i][j]);
+	}
+}
+}}
