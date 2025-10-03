9676218442af1e4f5b1f8d06f6a6aada64949805
Xcelevator-Sreeganesh <SreeGanesh.2531@xcelevate.org>
2025-10-03 19:50:34 +0530

    Update input.py

--- SreeGanesh.2531/Python/input.py
+++ SreeGanesh.2531/Python/input.py
@@ -1,31 +1,10 @@
-print():
+print("Hello "+ input("What is your name?"))..Input
+What is your name? SREE GANESH RAM
+Hello SREE GANESH RAM...Output
 
-print("Hello World!")..Input
+name=input("What is your name?")
+print(name)...I/p
+What is your name? Ganesh
+Ganesh ....O/p
 
-Hello World...Output
-
-print("Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
-print("Knead the dough for 10 minutes.")
-print("Add 3g of salt.")
-print("Leave to rise for 2 hrs.")
-print("Bake at 200 degrees c for 30 mins.") ....Input
-
-
-Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
-Knead the dough for 10 minutes.
-Add 3g of salt.
-Leave to rise for 2 hrs.
-Bake at 200 degrees c for 30 mins.....Output
-
-print("Notes from Day 1")
-print("The print statement is used to output strings")
-print("Strings are strings of characters")
-print("String Concatenation is done with the + sign")
-print("New lines can be created with a \ and the letter n")..Input
-
-Notes from Day 1
-The print statement is used to output strings
-Strings are strings of characters
-String Concatenation is done with the + sign
-New lines can be created with a \ and the letter n...Output
 
