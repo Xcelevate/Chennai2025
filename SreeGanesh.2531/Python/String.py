84a2cf58cd89ad7684fe67756989c0ae50bb1bc1
Xcelevator-Sreeganesh <SreeGanesh.2531@xcelevate.org>
2025-10-03 20:02:09 +0530

    Update String.py

--- SreeGanesh.2531/Python/String.py
+++ SreeGanesh.2531/Python/String.py
@@ -4,3 +4,21 @@ print("Hi"+" "+name+"\nIam learning Python") ...Input
 What is your name? Ganesh
 Hi Ganesh
 Iam learning Python...Output
+
+subscripting:
+name="Ganesh"
+print(name[0])..I/p
+G..O/p
+
+print(str[4])..I/p
+o..O/p
+print(str[-1])..I/p
+o..O/p
+
+slicing:
+str=("Hello")
+print(str[0:2])
+print(str[0:2:5])
+print(str[2:1:2])...I/p
+He
+H..O/p
