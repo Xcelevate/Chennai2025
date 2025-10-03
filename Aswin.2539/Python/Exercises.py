92b9fa739871bbb3f20b6404e0fc8a8a55338af9
XcelevatorAswin <aswin.2539@xcelevate.org>
2025-10-03 18:22:52 +0530

    Exercises.py

---
+++ Aswin.2539/Python/Exercises.py
@@ -0,0 +1,32 @@
+#Exercise1
+print("1.Mix 500g of Flour, 10g Yeast and 300ml water in bowl.")
+print("2.knead the dough for 10 minutes.")
+print("3.Add 3g of salt.")
+print("4.Leave to rise for 2 hours.")
+print("5.Bake at 200 degree C for 30  minutes.")
+
+
+#Exercise2
+print("Notes from Day 1") #unterminated string literal
+print("The print statemnet is used to output strings") #unexpected intend 
+print("Strings are strings of character") # incomplete input
+print("Strinig concatenation is done with the + sign") #name 'priint' is not defined
+print(("New lines can be created with a\ and the letter n")) #incomplete input
+
+
+
+#exercise3
+glass1="milk"
+glass2="juice"
+temp = glass1
+glass1=glass2
+glass2=temp
+print(glass1)
+
+
+#exercise3
+height = 1.65
+weight = 84
+c= weight / (height *height )
+print(round(c))
+
