81cfa4d4b89296aec1f7157ce13212d442826e42
XcelevatorMaheshwari <maheshwari.2517@xcelevate.org>
2025-10-03 19:44:28 +0530

    Print if the number have exactly 3 divisors

---
+++ Maheshwari.2517/Java/Flowcharts/FCP32.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class FCP32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 3) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
