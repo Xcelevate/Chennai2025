1da56c64cda582475250565b5176489c3c8a2a0a
XcelevatorMaheshwari <maheshwari.2517@xcelevate.org>
2025-10-04 07:08:33 +0530

    Natural Numbers in Array

---
+++ Maheshwari.2517/Java/Flowcharts/FCP053.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class FCP053{
+public static void main(String[]args){
+Scanner m =new Scanner(System.in);
+int size=m.nextInt();
+int arr[]=new int[size];
+int i;
+for(i=0;i<size;i++){
+arr[i]=i+1;
+     }
+for(int b=0;b<size;b++){
+System.out.println(arr[b]);
+}
+}
+}                                                                
