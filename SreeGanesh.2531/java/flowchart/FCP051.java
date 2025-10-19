47a0a4446c22d07cb0f3811621992983df0f3a95
Xcelevator-Sreeganesh <SreeGanesh.2531@xcelevate.org>
2025-10-19 09:34:21 +0530

    Add files via upload

---
+++ SreeGanesh.2531/java/flowchart/FCP051.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class FCP051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+		int s=0;
+		while(N>0){
+			s=s+(N%10);
+			N=N/10;
+		}
+		N=s;
+		if(s>10){
+			System.out.println(N);
+		}
+	}
+}
+			import java.util.Scanner;
+public class FCP051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+		int s=0;
+		while(N>0){
+			s=s+(N%10);
+			N=N/10;
+		}
+		N=s;
+		if(s>10){
+			System.out.println(N);
+		}
+	}
+}
+			import java.util.Scanner;
+public class FCP051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+		int s=0;
+		while(N>0){
+			s=s+(N%10);
+			N=N/10;
+		}
+		N=s;
+		if(s>10){
+			System.out.println(N);
+		}
+	}
+}
+			import java.util.Scanner;
+public class FCP051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+		int s=0;
+		while(N>0){
+			s=s+(N%10);
+			N=N/10;
+		}
+		N=s;
+		if(s>10){
+			System.out.println(N);
+		}
+	}
+}
+			import java.util.Scanner;
+public class FCP051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+		int s=0;
+		while(N>0){
+			s=s+(N%10);
+			N=N/10;
+		}
+		N=s;
+		if(s>10){
+			System.out.println(N);
+		}
+	}
+}
+			
