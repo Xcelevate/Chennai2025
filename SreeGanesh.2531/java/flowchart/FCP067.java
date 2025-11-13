788d426799bf07b395b9af2559f461aa2de4c4b3
Xcelevator-Sreeganesh <SreeGanesh.2531@xcelevate.org>
2025-11-13 18:29:10 +0530

    Add files via upload

---
+++ SreeGanesh.2531/java/flowchart/FCP067.java
@@ -0,0 +1,26 @@
+import java util.Scanner;
+public class Decrease{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int n=scn.nextInt();
+int []a=import java util.Scanner;
+public class Decrease{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int n=scn.nextInt();
+int []a=import java util.Scanner;
+public class Decrease{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int n=scn.nextInt();
+int []a=import java util.Scanner;
+public class Decrease{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int n=scn.nextInt();
+int []a=import java util.Scanner;
+public class Decrease{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int n=scn.nextInt();
+int []a=
