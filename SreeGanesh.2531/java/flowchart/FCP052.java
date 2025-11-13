93553e69e6de26d1d17afde9c2c7518319e7395f
Xcelevator-Sreeganesh <SreeGanesh.2531@xcelevate.org>
2025-11-13 18:21:09 +0530

    Add files via upload

---
+++ SreeGanesh.2531/java/flowchart/FCP052.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class FCP052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<=N;i++){
+		A[i] = sc.nextInt();
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}
+		import java.util.Scanner;
+
+public class FCP052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<=N;i++){
+		A[i] = sc.nextInt();
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}
+		import java.util.Scanner;
+
+public class FCP052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<=N;i++){
+		A[i] = sc.nextInt();
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}
+		import java.util.Scanner;
+
+public class FCP052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<=N;i++){
+		A[i] = sc.nextInt();
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}
+		import java.util.Scanner;
+
+public class FCP052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<=N;i++){
+		A[i] = sc.nextInt();
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}
+		
---
+++ SreeGanesh.2531/java/flowchart/FCP053.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class FCP053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<N;i++){
+		A[i]=i+1;
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}import java.util.Scanner;
+
+public class FCP053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<N;i++){
+		A[i]=i+1;
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}import java.util.Scanner;
+
+public class FCP053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<N;i++){
+		A[i]=i+1;
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}import java.util.Scanner;
+
+public class FCP053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<N;i++){
+		A[i]=i+1;
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}import java.util.Scanner;
+
+public class FCP053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<N;i++){
+		A[i]=i+1;
+		}
+		for(int j=0;j<N;j++){
+		System.out.println(A[j]);
+		}
+		}
+		}
