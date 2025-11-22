3c6e09ea637c4be89b8bd3fa507b81b46cedc054
XcelevatorLavanya <Lavanya.2515@xcelevate.org>
2025-11-22 21:07:02 +0530

    Add files via upload

---
+++ Lavanya.2515/python/FlowchartToCode/FCP014.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=int(input())
+c=int(input())
+"""
+if(a>b):
+    if(a>c):
+        print(a)
+    else:
+        print(c)
+elif(b>c):
+    print(b)
+else:
+    print(c)
+"""
+
+print(max(a,b,c))
---
+++ Lavanya.2515/python/FlowchartToCode/FCP018.py
@@ -0,0 +1,20 @@
+BS=int(input())
+
+if(BS<=10000):
+    HRA=BS*0.2
+    DA=BS*0.8
+    
+
+elif(BS<=20000):
+    HRA=BS*0.25
+    DA=BS*0.9
+    
+
+else:    
+    HRA=BS*0.3
+    DA=BS*0.95
+    
+CS=BS+HRA+DA
+print(int(CS))
+    
+   
